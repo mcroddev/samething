3c1c9167261c0c5617b0de2cb850ce94c7fe0002
Michael Rodriguez <me@mcrod.dev>
2023-06-03 13:10:14 -0400

    unix: improvements to toolchain shell script
    * Can now correctly handle verbose output redirection
    
    Signed-off-by: Michael Rodriguez <me@mcrod.dev>

--- toolchains/unix/build_toolchain.sh
+++ toolchains/unix/build_toolchain.sh
@@ -50,14 +50,27 @@ UNATTENDED=false
 # Do not suppress console output of external programs.
 VERBOSE=false
 
-# Immediately exit if any command fails.
-set -e
-
 directory_create() {
   if [ "$VERBOSE" = true ]; then
     mkdir -v "$1"
   else
     mkdir "$1"
+  fi
+}
+
+tarball_extract() {
+  if [ "$VERBOSE" = true ]; then
+    tar xvf "$1"
+  else
+    tar xf "$1"
+  fi
+}
+
+download_file() {
+  if [ "$VERBOSE" = true ]; then
+    curl -gLO "$1"
+  else
+    curl -gsLO "$1"
   fi
 }
 
@@ -81,6 +94,11 @@ usage() {
   echo "  -v, --verbose       Do not suppress console output of external "
   echo "                      programs; default is off."
 }
+
+if [ "$(id -u)" -eq 0 ]; then
+  >&2 echo "Refusing to run as root."
+  exit 1
+fi
 
 options=$(getopt -l "staging-dir:,target-dir:,use-lto,unattended,help,verbose" -o "s:t:luhv" -a -- "$@")
 
@@ -134,6 +152,20 @@ if [ -z "$TARGET_DIR" ]; then
   exit 1
 fi
 
+case $VERBOSE in
+  true)
+    V() {
+      "$@";
+    }
+    ;;
+
+  *)
+    V() {
+      "$@" > /dev/null;
+    }
+    ;;
+esac
+
 install_software_display() {
   echo "WARNING: This script requires the existence of certain software on your"
   echo "system to bootstrap the toolchain. It is highly recommended that you "
@@ -182,21 +214,20 @@ directory_create "${STAGING_DIR}"
 echo "Creating target directory ${TARGET_DIR}..."
 directory_create "${TARGET_DIR}"
 
-cd "${STAGING_DIR}" || return
-
-echo "Downloading CMake v${CMAKE_VER}..."
-
-curl -sLO "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VER}/cmake-${CMAKE_VER}.tar.gz"
-
-echo "Extracting CMake v${CMAKE_VER}..."
-
-if [ "$VERBOSE" = true ]; then
-  tar xvf "cmake-${CMAKE_VER}.tar.gz"
-else
-  tar xf "cmake-${CMAKE_VER}.tar.gz"
+if ! cd "${STAGING_DIR}"; then
+  exit 1
 fi
 
-cd cmake-${CMAKE_VER} || return
+echo "Downloading CMake v${CMAKE_VER}..."
+download_file "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VER}/cmake-${CMAKE_VER}.tar.gz"
+
+echo "Extracting CMake v${CMAKE_VER}..."
+tarball_extract "cmake-${CMAKE_VER}.tar.gz"
+
+if ! cd "cmake-${CMAKE_VER}"; then
+  exit 1
+fi
+
 echo "Configuring CMake v${CMAKE_VER}, please wait..."
 
 CMAKE_BUILD_FLAGS="-DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:STRING=""${TARGET_DIR}""/cmake"
@@ -205,17 +236,13 @@ if [ "$USE_LTO" = true ]; then
   CMAKE_BUILD_FLAGS="$CMAKE_BUILD_FLAGS -DCMake_BUILD_LTO:BOOL=ON"
 fi
 
-if [ "$VERBOSE" = true ]; then
-  cmake -S . -B build -G Ninja ${CMAKE_BUILD_FLAGS}
-else
-  cmake -S . -B build -G Ninja ${CMAKE_BUILD_FLAGS} 2>/dev/null
-fi
+V cmake -S . -B build -G Ninja $CMAKE_BUILD_FLAGS
 
 echo "Building and installing CMake v${CMAKE_VER}, this may take a while."
-cd build || return
 
-if [ "$VERBOSE" = true ]; then
-  ninja install
-else
-  ninja install 2>/dev/null
+if ! cd build; then
+  exit 1
 fi
+
+V ninja install
+echo "Installation of CMake v${CMAKE_VER} complete."
