7b7a07397fc9c94e2517108998f70f385267ac15
Michael Rodriguez <me@mcrod.dev>
2023-06-05 09:25:06 -0400

    samething: update toolchain script for unix
    The script is now more organized.
    
    Auto-detection of commands instead of a prompt asking the user if
    they've installed the required software. Almost fully portable.
    
    Signed-off-by: Michael Rodriguez <me@mcrod.dev>

--- toolchains/unix/build_toolchain.sh
+++ toolchains/unix/build_toolchain.sh
@@ -27,50 +27,48 @@
 
 # When pushing a commit to CI, it will check if any of these variables have
 # changed since the last commit. If they have, the toolchain is rebuilt.
-
-NINJA_VER="1.11.1"
-LLVM_VER="16.0.3"
-GCC_VER="13.0.1"
-CMAKE_VER="3.26.4"
-QT_VER="6.5.0"
+llvm_ver="16.0.5"
+gcc_ver="13.0.1"
+cmake_ver="3.26.4"
+ninja_ver="1.11.1"
+qt_ver="6.5.0"
 
 # The architectures which are supported by the toolchain.
-SUPPORTED_ARCHS="AArch64 X86"
+supported_archs="AArch64 X86"
 
-PROJECT_NAME="samething"
+project_name="samething"
 
 # Directory where the build takes place.
-STAGING_DIR=""
+staging_dir=""
 
 # Directory where the toolchain is stored.
-TARGET_DIR=""
-
-# Accept all prompts, useful for CI.
-UNATTENDED=false
+target_dir=""
 
 # Do not suppress console output of external programs.
-VERBOSE=false
+verbose=false
 
-directory_create() {
-  if [ "$VERBOSE" = true ]; then
-    mkdir -v "$1"
-  else
-    mkdir "$1"
-  fi
-}
+# Enable link-time optimization (LTO).
+use_lto=false
+
+# Enable profile-guided optimization (PGO).
+use_pgo=false
 
 tarball_extract() {
-  if [ "$VERBOSE" = true ]; then
-    tar xvf "$1"
-  else
-    tar xf "$1"
+  extract_flags="xf"
+
+  if [ "$verbose" = true ]; then
+    extract_flags="${extract_flags}v"
+  fi
+
+  if ! tar "${extract_flags}" "$1"; then
+    exit 1
   fi
 }
 
 download_file() {
   curl_flags="-gLO"
 
-  if [ "$VERBOSE" = false ]; then
+  if [ "$verbose" = false ]; then
     curl_flags="${curl_flags}s"
   fi
 
@@ -79,215 +77,267 @@ download_file() {
   fi
 }
 
+command_line_arguments_handle() {
+  # https://mywiki.wooledge.org/BashFAQ/035
+  while :; do
+    case $1 in
+      -h|-\?|--help)
+        usage 0
+        ;;
+
+      -s|--staging-dir)
+        if [ -z "$2" ]; then
+          printf '"--staging-dir" requires a path.\n' >&2
+	  exit 1
+	else
+          staging_dir=$2
+          shift
+	fi
+        ;;
+
+      --staging-dir=?*)
+        staging_dir=${1#*=}
+        ;;
+
+      --staging_dir=)
+        printf '"--staging-dir" requires a path.\n' >&2
+        exit 1
+        ;;
+
+      -t|--target-dir)
+        if [ -z "$2" ]; then
+          printf '"--target-dir" requires a path.\n' >&2
+          exit 1
+        else
+          target_dir=$2
+          shift
+        fi
+	;;
+
+      --target-dir=?*)
+        target_dir=${1#*=}
+	;;
+
+      --target-dir=)
+        printf '"--target-dir" requires a path.\n' >&2
+	exit 1
+	;;
+
+      -l|--use-lto)
+        use_lto=true
+        ;;
+
+      -p|--use-pgo)
+        use_pgo=true
+        ;;
+
+      -v|--verbose)
+        verbose=true
+	;;
+
+      --)
+        shift
+	break
+	;;
+
+      -?*)
+        printf 'Ignoring unknown option %s\n' "$1" >&2
+	;;
+
+      *)
+        break
+    esac
+    shift
+  done
+
+  if [ -z "$staging_dir" ]; then
+    >&2 echo "Staging directory not specified."
+    usage 1
+  fi
+
+  if [ -z "$target_dir" ]; then
+    >&2 echo "Target directory not specified."
+    usage 1
+  fi
+
+  case $verbose in
+    true)
+      V() {
+        "$@";
+      }
+      ;;
+
+      *)
+        V() {
+          "$@" > /dev/null;
+        }
+        ;;
+  esac
+}
+
+cmake_build() {
+  echo "Downloading CMake v${cmake_ver}..."
+  download_file "https://github.com/Kitware/CMake/releases/download/v${cmake_ver}/cmake-${cmake_ver}.tar.gz"
+
+  echo "Extracting CMake v${cmake_ver}..."
+  tarball_extract "cmake-${cmake_ver}.tar.gz"
+
+  if ! cd "cmake-${cmake_ver}"; then
+    exit 1
+  fi
+
+  echo "Configuring CMake v${cmake_ver}, please wait..."
+
+  if [ "$use_lto" = true ]; then
+    set -- "$@" "-DCMake_BUILD_LTO:BOOL=ON"
+  fi
+
+  if ! V cmake -S . -B build -G Ninja "$@"; then
+    exit 1
+  fi
+
+  echo "Building and installing CMake v${cmake_ver}, this may take a while."
+
+  if ! cd build; then
+    exit 1
+  fi
+
+  if ! V ninja install; then
+    exit 1
+  fi
+
+  echo "Installation of CMake v${cmake_ver} complete."
+
+  if ! cd "${staging_dir}"; then
+    exit 1
+  fi
+}
+
+ninja_build() {
+  echo "Downloading ninja v${ninja_ver}..."
+  download_file "https://github.com/ninja-build/ninja/archive/refs/tags/v${ninja_ver}.tar.gz"
+
+  echo "Extracting ninja v${ninja_ver}..."
+  tarball_extract "v${ninja_ver}.tar.gz"
+
+  if ! cd "ninja-${ninja_ver}"; then
+    exit 1
+  fi
+
+  # Regardless of whether or not LTO is enabled by this script, Ninja will
+  # enforce LTO if the compiler supports it (which it should). This shouldn't
+  # cause harm; ninja is quite small to begin with.
+  ninja_build_flags="-DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:STRING=""${target_dir}""/ninja"
+
+  echo "Configuring ninja v${ninja_ver}, please wait..."
+
+  if ! V cmake -S . -B build -G Ninja $ninja_build_flags; then
+    exit 1
+  fi
+
+  echo "Building and installing ninja v${ninja_ver}, this may take a while."
+
+  if ! cd build; then
+    exit 1
+  fi
+
+  if ! V ninja install; then
+    exit 1
+  fi
+
+  echo "Installation of ninja v${ninja_ver} complete."
+
+  if ! cd "${staging_dir}"; then
+    exit 1
+  fi
+}
+
+llvm_build() {
+  echo "Downloading LLVM ${llvm_ver}..."
+  download_file "https://github.com/llvm/llvm-project/releases/download/llvmorg-${llvm_ver}/llvm-project-${llvm_ver}.src.tar.xz"
+
+  echo "Extracting LLVM ${llvm_ver}..."
+  tarball_extract "llvm-project-${llvm_ver}.src.tar.xz"
+}
+
 usage() {
-  echo "Usage: ./build_toolchain.sh [OPTIONS]"
-  echo
-  echo "Builds a toolchain suitable for development and deployment of "
-  echo "$PROJECT_NAME."
-  echo
-  echo "Required arguments: "
-  echo
-  echo "  --staging-dir=STAGINGDIR    Directory where the build takes place."
-  echo "  --target-dir=TARGETDIR      Directory where the toolchain is stored."
-  echo
-  echo "Optional arguments:"
-  echo "  -u, --unattended    Accept all prompts, useful for CI. Default is off."
-  echo "  --use-lto           Compile the toolchain with link-time optimization"
-  echo "                      enabled. The toolchain will be faster, but the "
-  echo "                      build time will be much slower and much more"
-  echo "                      intensive on the host. Default is off."
-  echo "  -v, --verbose       Do not suppress console output of external "
-  echo "                      programs; default is off."
+  cat << EOF
+Usage: ./build_toolchain.sh [OPTIONS]
+
+Builds a toolchain suitable for development and deployment of ${project_name}.
+
+Required arguments:
+
+  --staging-dir=STAGINGDIR    Directory where the build takes place.
+  --target-dir=TARGETDIR      Directory where the toolchain is stored.
+
+Optional arguments:
+  -l, --use-lto       Compile the toolchain with link-time optimization
+                      where appropriate. The toolchain will be faster,
+                      but the build time will be much slower and much
+                      more intensive on the host. Default is off.
+  -p, --use-pgo       Compile the toolchain with profile-guided
+                      optimization where appropriate. The toolchain
+		      will be faster, but the build time will be much
+                      slower and much more intensive on the host.
+                      Default is off.
+  -v, --verbose       Do not suppress console output of external
+                      programs; default is off.
+EOF
+  exit "$1"
+}
+
+check_required() {
+  for required in cmake ninja gcc clang; do
+    if command -v "$required" >/dev/null; then
+      echo "Required command ${required} not found."
+      "$required" >&2
+      set -- "$@"
+      "$required"
+    fi
+  done
+
+  if [ $# -gt 0 ]; then
+    cat <<-EOF >&2
+ERROR: One or more required software packages were not detected on your system.
+This software is necessary to bootstrap the toolchain. It is highly recommended
+that you consult your distribution's package manager to install the required
+software. This script will not install the software for you as there are too
+many package managers to account for to reasonably automate this process. If you
+are unable to install the required software, your only alternative is to
+download a prebuilt toolchain.
+
+Required software:
+
+* CMake - https://cmake.org/
+* GCC   - https://gcc.gnu.org/
+* LLVM  - https://llvm.org/
+* Ninja - https://ninja-build.org/
+EOF
+  fi
 }
 
 if [ "$(id -u)" -eq 0 ]; then
-  >&2 echo "Refusing to run as root."
+  cat <<-EOF >&2
+WARNING: You appear to be running this script as a superuser. It is
+adviseable, though not required, to run this script as a non-privileged user.
+
+EOF
+fi
+
+command_line_arguments_handle "$@"
+check_required
+
+staging_dir=$(realpath "${staging_dir}")
+target_dir=$(realpath "${target_dir}")
+
+mkdir -v "${staging_dir}"
+mkdir -v "${target_dir}"
+
+if ! cd "${staging_dir}"; then
   exit 1
 fi
 
-options=$(getopt -l "staging-dir:,target-dir:,use-lto,unattended,help,verbose" -o "s:t:luhv" -a -- "$@")
-
-eval set -- "$options"
-
-while true
-  do
-    case "$1" in
-    -s|--staging-dir)
-      shift
-      STAGING_DIR="$1"
-      ;;
-
-    -t|--target-dir)
-      shift
-      TARGET_DIR="$1"
-      ;;
-
-    -l|--use-lto)
-      USE_LTO=true
-      ;;
-
-    -u|--unattended)
-      UNATTENDED=true
-      ;;
-
-    -h|--help)
-      usage
-      exit 0
-      ;;
-
-    -v|--verbose)
-      VERBOSE=true
-      ;;
-    --)
-      shift
-      break;;
-  esac
-  shift
-done
-
-if [ -z "$STAGING_DIR" ]; then
-  >&2 echo "Staging directory not specified."
-  usage
-  exit 1
-fi
-
-if [ -z "$TARGET_DIR" ]; then
-  >&2 echo "Target directory not specified."
-  usage
-  exit 1
-fi
-
-case $VERBOSE in
-  true)
-    V() {
-      "$@";
-    }
-    ;;
-
-  *)
-    V() {
-      "$@" > /dev/null;
-    }
-    ;;
-esac
-
-install_software_display() {
-  echo "WARNING: This script requires the existence of certain software on your"
-  echo "system to bootstrap the toolchain. It is highly recommended that you "
-  echo "consult your distribution's package manager to install the required "
-  echo "software. This script will not install the software for you as there"
-  echo "are too many package managers to account for to reasonably automate"
-  echo "this process. If you are unable to install the required software, your "
-  echo "only alternative is to download a prebuilt toolchain."
-  echo
-  echo "Required software:"
-  echo
-  echo "* CMake - https://cmake.org/"
-  echo "* GCC   - https://gcc.gnu.org/"
-  echo "* LLVM  - https://llvm.org/"
-  echo "* Ninja - https://ninja-build.org/"
-  echo
-}
-
-if [ "$UNATTENDED" = false ]; then
-  install_software_display
-
-  while true
-  do
-    printf "Have you installed this software? [Y/N] "
-    read -r REPLY
-
-    if expr "$REPLY" : '^[Yy]' 1>/dev/null; then
-      echo
-      break
-    elif expr "$REPLY" : '^[Nn]' 1>/dev/null; then
-      >&2 echo "Aborting due to required software not being installed."
-      exit 1
-    else
-      >&2 echo "Invalid input; please press Y or N, or press Ctrl+C to exit."
-    fi
-  done
-fi
-
-# Resolve relative to absolute paths.
-STAGING_DIR=$(realpath "${STAGING_DIR}")
-TARGET_DIR=$(realpath "${TARGET_DIR}")
-
-echo "Creating staging directory ${STAGING_DIR}..."
-directory_create "${STAGING_DIR}"
-
-echo "Creating target directory ${TARGET_DIR}..."
-directory_create "${TARGET_DIR}"
-
-if ! cd "${STAGING_DIR}"; then
-  exit 1
-fi
-
-echo "Downloading CMake v${CMAKE_VER}..."
-download_file "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VER}/cmake-${CMAKE_VER}.tar.gz"
-
-echo "Extracting CMake v${CMAKE_VER}..."
-tarball_extract "cmake-${CMAKE_VER}.tar.gz"
-
-if ! cd "cmake-${CMAKE_VER}"; then
-  exit 1
-fi
-
-echo "Configuring CMake v${CMAKE_VER}, please wait..."
-
-CMAKE_BUILD_FLAGS="-DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:STRING=""${TARGET_DIR}""/cmake"
-
-if [ "$USE_LTO" = true ]; then
-  CMAKE_BUILD_FLAGS="$CMAKE_BUILD_FLAGS -DCMake_BUILD_LTO:BOOL=ON"
-fi
-
-V cmake -S . -B build -G Ninja $CMAKE_BUILD_FLAGS
-
-echo "Building and installing CMake v${CMAKE_VER}, this may take a while."
-
-if ! cd build; then
-  exit 1
-fi
-
-if ! V ninja install; then
-  exit 1
-fi
-
-echo "Installation of CMake v${CMAKE_VER} complete."
-
-if ! cd "${STAGING_DIR}"; then
-  exit 1
-fi
-
-echo "Downloading ninja v${NINJA_VER}..."
-download_file "https://github.com/ninja-build/ninja/archive/refs/tags/v${NINJA_VER}.tar.gz"
-
-echo "Extracting ninja v${NINJA_VER}..."
-tarball_extract "v${NINJA_VER}.tar.gz"
-
-if ! cd "ninja-${NINJA_VER}"; then
-  exit 1
-fi
-
-# Regardless of whether or not LTO is enabled by this script, Ninja will enforce
-# LTO if the compiler supports it (which it should). This shouldn't cause harm,
-# ninja is quite small to begin with.
-NINJA_BUILD_FLAGS="-DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:STRING=""${TARGET_DIR}""/ninja"
-
-echo "Configuring ninja v${NINJA_VER}, please wait..."
-
-if ! V cmake -S . -B build -G Ninja $NINJA_BUILD_FLAGS; then
-  exit 1
-fi
-
-echo "Building and installing ninja v${NINJA_VER}, this may take a while."
-
-if ! cd build; then
-  exit 1
-fi
-
-if ! V ninja install; then
-  exit 1
-fi
-
-echo "Installation of ninja v${NINJA_VER} complete."
+cmake_build -DCMAKE_BUILD_TYPE:STRING=Release "-DCMAKE_INSTALL_PREFIX:STRING=${target_dir}/cmake"
+ninja_build
+llvm_build
